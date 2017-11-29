b1ec74cda57a8510f00feb9c2e3e82db6a0a0333
Maurice Roy <mautayro@gmail.com>
2017-11-29 16:56:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
 def oxford_comma(array)
   output = array.join
-  if array.length > 1
+  if array.length > 2
     last_word = array.pop
     output = array.join(", ")
     output << ", and #{last_word}"
+  elsif
+    array.lenth == 2
+    output = array.join(' and ')
   end
   output
 end
