0749c3ebbbab64e17f2aef248d42b514a0d6cc8c
Maurice Roy <mautayro@gmail.com>
2017-11-29 16:54:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  last_word = array.pop
-  output = array.join(", ")
-  output << " and #{last_word}"
+  output = array.to_s
+  if array.length > 1
+    last_word = array.pop
+    output = array.join(", ")
+    output << " and #{last_word}"
+  end
+  output
 end
